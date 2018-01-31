f45023eb974eed0f4fcc121c19ec811ac187c8c0
garevna <garevna@mail.ru>
2018-01-31 17:41:42 +0200

    Update VueInstance.js

--- VueInstance.js
+++ VueInstance.js
@@ -36,7 +36,7 @@ const app = new Vue ( {
 	mounted: function () {
 		console.log ( 'VUE instance: firebaseAuthObject ', this.firebaseAuthObject )
 		console.log ( '*** ', this.firebaseAuthObject.currentUser )
-		this.firebaseAuthObject.addEventListener ( 'AuthStateChanged', function ( user ) {
+		public void addIdTokenListener ( FirebaseAuth.IdTokenListener function ( user ) {
 			if ( user ) {
 				console.log ( 'VUE instance: provider data: ', user.providerData )
 				this.firebaseUser = user.providerData
