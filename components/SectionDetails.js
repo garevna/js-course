03af672fb0d377e0e04a660c79009d2ebc07d0c5
garevna <garevna@mail.ru>
2018-02-02 14:02:49 +0200

    Update SectionDetails.js

--- components/SectionDetails.js
+++ components/SectionDetails.js
@@ -26,7 +26,7 @@ const SectionDetails = {
 
   },
   mounted: function () {
-      console.log ( 'SectionDetails this.$root.$route.path:', this.$root.$route.path )
+      
   },
   template: `
     <section>
@@ -35,27 +35,8 @@ const SectionDetails = {
                       v-model = "currentPostId"
                       @change = "console.log ('currentPostId')">
         </select-post>
-        <router-view class = "router-view" :postId = "currentPostId">
+        <router-view :postId = "currentPostId">
         </router-view>
     </section>
-
-    <!--
-      <v-expansion-panel focusable v-if = "sectionIsReady">
-        <v-expansion-panel-content
-                :key = "index">
-            <div class="title" slot = "header">
-              {{ item.head }}
-            </div>
-            <v-card>
-              <v-card-text>
-                  <current-post
-                      :index = "index"
-                      :postObject = "item">
-                  </current-post>
-              </v-card-text>
-            </v-card>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-    </v-app>-->
     `
 }
