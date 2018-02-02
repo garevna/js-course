e944696633a71a56f2b9b2fcde288bc8264d9fb1
garevna <garevna@mail.ru>
2018-02-02 12:38:58 +0200

    Update firebaseUserAuth.js

--- firebaseUserAuth.js
+++ firebaseUserAuth.js
@@ -17,59 +17,21 @@ const messagesRef = firebaseDB.ref ( 'message' )
 console.log ( 'firebaseApp usersRef: ', usersRef )
 console.log ( 'firebaseApp messagesRef: ', messagesRef )
 
-var uiConfig = {
-        signInSuccessUrl: '/vue-course.github.io/#/',
-        signInOptions: [
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-            firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-            firebase.auth.GithubAuthProvider.PROVIDER_ID,
-            firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        ],
+//var uiConfig = {
+//        signInSuccessUrl: '/vue-course.github.io/#/',
+//        signInOptions: [
+//            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+//            firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+//            firebase.auth.GithubAuthProvider.PROVIDER_ID,
+//            firebase.auth.EmailAuthProvider.PROVIDER_ID,
+//        ],
         // Terms of service url
-        tosUrl: '/vue-course.github.io/'
-}
+//        tosUrl: '/vue-course.github.io/'
+//}
 
-var ui = new firebaseui.auth.AuthUI ( firebase.auth () )
-console.log ( 'instance of firebaseui.auth.AuthUI: ', ui )
+//var ui = new firebaseui.auth.AuthUI ( firebase.auth () )
+//console.log ( 'instance of firebaseui.auth.AuthUI: ', ui )
 
 // The start method will wait until the DOM is loaded
 
-ui.start( '#firebaseui-auth-container', uiConfig )
-
-initApp = function () {
-    firebase.auth().onAuthStateChanged ( function ( user ) {
-       if ( user ) {
-            var displayName = user.displayName
-            var email = user.email
-            var emailVerified = user.emailVerified
-            var photoURL = user.photoURL
-            var uid = user.uid
-            var phoneNumber = user.phoneNumber
-            var providerData = user.providerData
-            user.getIdToken().then ( function ( accessToken ) {
-                      console.log ( 'app.sendMessage: ', app.sendMessage )
-                      console.log ( 'accessToken: ', accessToken )
-                //document.getElementById('sign-in-status').textContent = 'Signed in';
-                //document.getElementById('sign-in').textContent = 'Sign out';
-                //document.getElementById('account-details').textContent = JSON.stringify({
-                //displayName: displayName,
-                //email: email,
-                //emailVerified: emailVerified,
-                //phoneNumber: phoneNumber,
-                //photoURL: photoURL,
-                //uid: uid,
-                //accessToken: accessToken,
-                //providerData: providerData
-              //}, null, '  ')
-            })
-          } else {
-            // User is signed out
-                    console.log ( 'User is signed out', accessToken )
-            document.getElementById('sign-in-status').textContent = 'Signed out'
-            document.getElementById('sign-in').textContent = 'Sign in'
-            document.getElementById('account-details').textContent = 'null'
-          }
-        }, function ( error ) {
-            console.log ( error )
-        })
-}
+//ui.start( '#firebaseui-auth-container', uiConfig )
