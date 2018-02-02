ade54774ac7ec360de7650bc30762d4e3ab06aae
garevna <garevna@mail.ru>
2018-02-02 12:19:44 +0200

    Update loginComponent.js

--- components/loginComponent.js
+++ components/loginComponent.js
@@ -1,70 +1,74 @@
-const SignInComponent = ( 'sign-in-with-email', {
+const LoginComponent = ( 'login-component', {
     data: function () {
         return {
-            password: null,
-            email: '',
-            alert: false
+            dialog: false,
+            loginForm: false
         }
     },
     computed: {
-        error: () => this.$store.getters.getError,
-        loading: () => this.$store.getters.getLoading
-    },
-    watch: {
-        error: value => { this.alert = !!value },
-        alert: value => {
-            value ? return : this.$store.dispatch ( 'setError', false )
-        }
-    },
-    methods: {
-        userSignIn () {
-            this.$root.$store.dispatch ( 'userSignIn', {
-                  email: this.email,
-                  password: this.password
-            })
-        }
+        firebaseAuthObject: function () {
+            return this.$root.firebaseAuthObject
+        },
+        currentUser: function () {
+            if ( this.$root.firebaseAuthObject )
+                return this.$root.firebaseAuthObject.currentUser
+            else return null
+        },
+        currentProvider: function () {
+            if ( this.$root.firebaseAuthObject && this.$root.firebaseAuthObject.currentUser )
+                var user = this.$root.firebaseAuthObject.currentUser
+                if ( user.providerData ) return user.providerData[0].providerId
+            else return null
+        },
     },
     template: `
-      <v-layout column>
-        <v-flex xs12 class="text-xs-center" mt-5>
-          <h3>Sign In</h3>
-        </v-flex>
-        <v-flex xs12 sm6 offset-sm3 mt-3>
-          <form @submit.prevent = "userSignIn">
-            <v-layout column>
-              <v-flex>
-                  <v-alert error dismissible v-model = "alert">
-                        {{ error }}
-                  </v-alert>
-              </v-flex>
-              <v-flex>
-                  <v-text-field
-                      name = "email"
-                      label = "Email"
-                      id = "email"
-                      type = "email"
-                      v-model = "email"
-                      required>
-                  </v-text-field>
-              </v-flex>
-              <v-flex>
-                  <v-text-field
-                      name = "password"
-                      label = "Password"
-                      id = "password"
-                      type = "password"
-                      v-model = "password"
-                      required>
-                  </v-text-field>
-              </v-flex>
-              <v-flex class="text-xs-center" mt-5>
-                  <v-btn icon primary type="submit" :disabled="loading">
-                        <v-icon>exit_to_app</v-icon>
-                  </v-btn>
-              </v-flex>
-            </v-layout>
-          </form>
-        </v-flex>
-      </v-layout>
-    `
+        <v-layout row justify-center v-if = "firebaseAuthObject">
+                    <v-btn color = "primary" 
+                           dark icon
+                           v-if = "currentUser"
+                           @click.native.stop = "dialog = true">
+                        <v-avatar size = "36px" 
+                                  v-if = "currentUser.photoURL">
+                            <img :src = "currentUser.photoURL" 
+                                 :alt = "currentUser.displayName">
+                        </v-avatar>
+                        <v-icon v-else>person</v-icon>
+                    </v-btn>
+                    
+                    <v-dialog v-model = "dialog" max-width = "290">
+                            <v-card>
+                                <v-card-title class = "headline"
+                                              v-text = "currentUser.providerData[0].providerId">
+                                </v-card-title>
+                                <v-card-text v-text = "firebaseAuthObject.currentUser.displayName"></v-card-text>
+                                <v-card-text v-text = "firebaseAuthObject.currentUser.email"></v-card-text>
+                                <v-card-text v-text = "firebaseAuthObject.currentUser.phoneNumber"></v-card-text>
+                                <v-card-text v-text = "firebaseAuthObject.currentUser.metadata.lastSignInTime"></v-card-text>
+                                
+                                <v-card-actions>
+                                    <v-spacer></v-spacer>
+                                    <v-btn  color = "info darken-1" 
+                                            flat = "flat" 
+                                            @click.native = "dialog = false">
+                                        Close
+                                    </v-btn>
+                                    <v-btn  color = "error darken-1" 
+                                            flat = "flat" 
+                                            @click.native = "dialog = false; userLogOut">
+                                        Sign out
+                                    </v-btn>
+                                </v-card-actions>
+                            </v-card>
+                        </v-dialog>
+                </v-layout>
+                
+                <v-btn color = "primary" 
+                           dark icon
+                           v-if = "!currentUser"
+                           @click.native.stop = "loginForm = true">
+                        <v-icon v-else>perm_identity</v-icon>
+                </v-btn>
+                <div id = "firebaseui-auth-container" v-show = "loginForm"></div>
+    `,
+    
 })
