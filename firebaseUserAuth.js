2a9f11fb4f6f16a4f04671add43b7c47c8e2cadb
garevna <garevna@mail.ru>
2018-02-11 13:16:25 +0200

    Update firebaseUserAuth.js

--- firebaseUserAuth.js
+++ firebaseUserAuth.js
@@ -13,25 +13,3 @@ const firebaseApp = firebase.initializeApp ( config )
 const firebaseDB = firebaseApp.database()
 const usersRef = firebaseDB.ref ( 'users' )
 const messagesRef = firebaseDB.ref ( 'message' )
-
-console.log ( 'firebaseApp usersRef: ', usersRef )
-console.log ( 'firebaseApp messagesRef: ', messagesRef )
-
-//var uiConfig = {
-//        signInSuccessUrl: '/vue-course.github.io/#/',
-//        signInOptions: [
-//            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-//            firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-//            firebase.auth.GithubAuthProvider.PROVIDER_ID,
-//            firebase.auth.EmailAuthProvider.PROVIDER_ID,
-//        ],
-        // Terms of service url
-//        tosUrl: '/vue-course.github.io/'
-//}
-
-//var ui = new firebaseui.auth.AuthUI ( firebase.auth () )
-//console.log ( 'instance of firebaseui.auth.AuthUI: ', ui )
-
-// The start method will wait until the DOM is loaded
-
-//ui.start( '#firebaseui-auth-container', uiConfig )
