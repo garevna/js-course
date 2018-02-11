acc29d2b0bcb61f6ce5781d1d09a3a800be0688f
garevna <garevna@mail.ru>
2018-02-11 13:13:51 +0200

    Update DataStore.js

--- DataStore.js
+++ DataStore.js
@@ -69,38 +69,9 @@ const store = new Vuex.Store ({
     }
   },
   actions: {
-    userSignUp ( { commit }, payload ) {
-        commit ( 'setLoading', true )
-        firebase.auth().createUserWithEmailAndPassword ( payload.email, payload.password )
-            .then ( firebaseUser => {
-                firebaseUser.sendEmailVerification ()
-                    .then ( () => { console.log('welcome email has been sent') } )
-                commit ( 'setUser', firebaseUser )
-                commit ( 'setLoading', false )
-                commit ( 'setError', null )
-                router.push ( '/home' )
-            })
-            .catch(error => {
-                commit ( 'setError', error.message )
-                commit ( 'setLoading', false )
-            })
-    },
-    userSignIn ( { commit }, payload ) {
-      commit('setLoading', true)
-          firebase.auth().signInWithEmailAndPassword ( payload.email, payload.password )
-              .then ( firebaseUser => {
-                  commit( 'setUser', firebaseUser )
-                  commit( 'setLoading', false )
-                  commit( 'setError', null )
-                  router.push ( '/home' )
-              })
-              .catch ( error => {
-                  commit ( 'setError', error.message )
-                  commit ( 'setLoading', false )
-      })
-    }
+    
   },
   mounted: function () {
-    console.log ( this.currentSectionId )
+    
   }
 })
