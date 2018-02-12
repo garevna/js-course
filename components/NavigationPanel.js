94a73ee90f42ad6233d2deb953d34a1deb2c66eb
garevna <garevna@mail.ru>
2018-02-12 16:57:45 +0200

    Update NavigationPanel.js

--- components/NavigationPanel.js
+++ components/NavigationPanel.js
@@ -7,18 +7,21 @@ const NavigationPanel = ( 'nav-panel', {
     },
     template: `
       <v-layout wrap justify-left>
-        <v-btn  icon  large dark
-                @click.stop = "drawer = !drawer">
+        <v-btn  icon  large
+                @click.stop = "drawer = !drawer"
+                dark color = "transparent">
             <v-icon>menu</v-icon>
         </v-btn>
         <v-navigation-drawer  temporary
                               v-model = "drawer"
                               clipped = true
+                              height = "80%"
+                              class = "warning"
                               fixed>
-          <v-list class="pa-1">
-            <v-list-tile>
+          <v-list class = "pa-1 accent">
+            <v-list-tile class = "accent">
                 <v-list-tile-avatar>
-                    <img src="./images/vue.svg" />
+                    <img src = "/images/vue.svg" />
                 </v-list-tile-avatar>
                 <v-list-tile-content>
                     <v-list-tile-title>
@@ -28,16 +31,17 @@ const NavigationPanel = ( 'nav-panel', {
             </v-list-tile>
           </v-list>
 
-          <v-list class = "warning pt-0" dense>
+          <v-list class="pt-0 accent" dense>
             <v-divider></v-divider>
             <v-list-tile v-for = "item in states"
                          :key = "item"
+                         class = "warning"
                          @click = "clickHandler ( event, item )">
                   <v-list-tile-action>
-                        <v-icon>assignment</v-icon>
+                        <v-icon class = "white--text">assignment</v-icon>
                   </v-list-tile-action>
                   <v-list-tile-content>
-                        <v-list-tile-title>
+                        <v-list-tile-title class = "white--text">
                             {{ item }}
                         </v-list-tile-title>
                   </v-list-tile-content>
@@ -48,6 +52,7 @@ const NavigationPanel = ( 'nav-panel', {
     `,
     methods: {
         clickHandler: function ( event, val ) {
+          console.info ( 'NavigationPanel: ', val )
           this.$root.$store.commit( 'changeCurrentSectionId', val )
           this.$root.$store.commit( 'getCurrentSectionInfo' )
           this.$root.$store.commit( 'getCurrentSectionPosts' )
@@ -55,6 +60,6 @@ const NavigationPanel = ( 'nav-panel', {
         }
     },
     mounted: function () {
-      
+      console.log ( 'NavigationPanel menuItems: ', this.states )
     }
 })
