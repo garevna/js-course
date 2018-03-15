cb0ad74c1fada5a26503bad01612f81d4056d3ca
garevna <garevna@mail.ru>
2018-03-15 09:45:35 +0200

    Update toggleButtons.js

--- components/toggleButtons.js
+++ components/toggleButtons.js
@@ -2,51 +2,38 @@ const ToggleButtons = ( 'toggle-buttons', {
   props: [ '__buttons' ],
   data: function () {
       return {
-        store: this.$root.$store,
-        route: this.$root.$route,
-        val: undefined,
-        toggle_exclusive: null
+        vals: [ 'about', 'details' ],
+        toggle_none: null
       }
   },
   watch: {
-    val: function () {
-        //var __name = this.val === 0 ? 'about' : 'details'
-        //this.$root.$router.push ( { name: __name } )
-    },
-    routeName: function ( newVal, oldVal ) {
-        this.toggle_exclusive = this.calcToggleNum ( newVal )
+    toggle_none: function ( newVal, oldVal ) {
+      if ( newVal !== null )
+        this.$root.$router.push ( { name: this.vals [ newVal ] } )
     }
   },
   computed: {
-    routeName: function () { 
-        this.toggle_exclusive = this.calcToggleNum ( this.$route.name )
-        return this.$route.name
-    },
     toggleButtonsVisibility: function () {
       return this.$root.$store.getters.sectionIsReady
     }
   },
   template: `
         <v-btn-toggle dark v-if = "toggleButtonsVisibility"
-                      v-model="toggle_exclusive"
+                      v-model="toggle_none"
                       class="py-2">
-              <v-btn  outline  replace = true
-                      @click = "router.push ( { name: 'about' } )">
+              <v-btn  outline
+                      @click = "$root.$router.push ( { name: 'about' } )">
                   <v-icon>description</v-icon>
               </v-btn>
-              <v-btn  outline  replace = true
-                      @click = "router.push ( { name: 'details' } )">
+              <v-btn  outline
+                      @click = "$root.$router.push ( { name: 'details' } )">
                   <v-icon>dashboard</v-icon>
               </v-btn>
         </v-btn-toggle>
   `,
   mounted: function () {
-      this.val = this.toggle_exclusive
-  },
-  methods: {
-      calcToggleNum: function ( routeName ) {
-          return routeName === 'about' ? 0 :
-                  ( routeName === 'details' || routeName === 'section' ? 1 : null )
-      }
+    console.log ( '$route.path: ', this.$route.path )
+    this.toggle_none = this.$route.path.indexOf ( this.vals [0] ) ? 0 :
+                        ( this.$route.path.indexOf ( this.vals [1] ) ? 1 : null )
   }
 })
